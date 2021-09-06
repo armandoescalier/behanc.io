461dcdc470ff2c3dda4d1682465a9f75cdedb2c0
armando escalier <armando.escalier@michelada.io>
2021-09-06 19:27:05 +0000

    Add user can see other public profiles

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,8 @@
-class ProfileController < AuthorizationsController
+class ProfileController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
+    @presenter = UserPresenter.new(@user, current_user)
+
+    redirect_to root_path, flash: { alert: "The user #{params[:username]} was not found" } unless @user
   end
 end
