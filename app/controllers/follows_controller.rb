49d96496c9c1037c10e448d6d1a2ed2216eb3a24
armando escalier <armando.escalier@michelada.io>
2021-09-14 16:32:48 -0500

    Changes at Follows controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,18 +5,22 @@ class FollowsController < ApplicationController
     if current_user.followings.exclude?(@user)
       current_user.followings << @user
       flash[:notice] = "Now you follow #{@user.username}"
+    else
+      flash[:alert] = "You already follow #{@user.username}"
     end
 
-    redirect_back(fallback_location: profile_path(@user)) unless current_user.followings.exclude?(@user)
+    redirect_back(fallback_location: profile_path(@user))
   end
 
   def destroy
     if current_user.followings.include?(@user)
       current_user.given_follows.find_by(followed_user_id: @user.id).destroy
-      flash[:notice] = "You have unfollow #{@user.username}"
+      flash[:notice] = "You have unfollowed #{@user.username}"
+    else
+      flash[:alert] = "You are not following #{@user.username}"
     end
 
-    redirect_back(fallback_location: profile_path(@user)) unless current_user.followings.include?(@user)
+    redirect_back(fallback_location: profile_path(@user))
   end
 
   def set_user
