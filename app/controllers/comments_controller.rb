c46796303dfb242b14777c97f6d20995294d7883
armando escalier <armando.escalier@michelada.io>
2021-09-16 12:05:10 -0500

    Fixed comments MR

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
-class CommentsController < ApplicationController
+class CommentsController < AuthorizationsController
+  before_action :set_comment
+
   def create
     @comment = current_user.comments.new(comment_params)
 
-    if @comment
-      @comment.save
+    if @comment.save
       flash[:notice] = 'Comment created'
     else
       flash[:alert] = 'Comment cannot be created'
@@ -13,19 +14,24 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = current_user.comments.find(params[:id])
-
-    if @comment
-      @comment.destroy
-      flash[:notice] = 'Comment deleted'
+    if @comment.present?
+      if @comment.destroy
+        flash[:notice] = 'Comment deleted'
+      else
+        flash[:alert] = 'Comment cannot be deleted'
+      end
     else
-      flash[:alert] = 'Comment cannot be deleted'
+      flash[:alert] = 'Comment not found'
     end
 
     redirect_to project_path(params[:project_id])
   end
 
   private
+
+  def set_comment
+    @comment = current_user.comments.find_by_id(params[:id])
+  end
 
   def comment_params
     params
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -3,8 +3,26 @@ require 'test_helper'
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   test 'logged user can make a comment' do
     sign_in users(:joe)
+
     post project_comments_path(projects(:one), params: { comment: { content: 'hi' } })
 
     assert_equal 'Comment created', flash[:notice]
+  end
+
+  test 'success if comment deleted' do
+    comment = comments(:one)
+    sign_in comment.user
+
+    delete project_comment_path(comment.project, comment)
+
+    assert_equal 'Comment deleted', flash[:notice]
+  end
+
+  test 'fails if comment not found' do
+    sign_in users(:joe)
+
+    delete project_comment_path(projects(:one), 25)
+
+    assert_equal 'Comment not found', flash[:alert]
   end
 end
